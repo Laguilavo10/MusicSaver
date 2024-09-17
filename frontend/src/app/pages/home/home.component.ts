f4040edc42aac8ddf4886a30f143962915e08f59
Andres Laguilavo <lagui2003@gmail.com>
2024-09-16 20:02:56 -0500

    Refactor home component to display user's liked tracks

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -7,7 +7,26 @@ import { Component } from '@angular/core'
   imports: [TracksComponent],
   template: `
     @if (access_token) {
-    <tracks />
+    <div class='flex flex-col gap-10'>
+      <div class="flex gap-5">
+        <div class="size-40 bg-gradient-to-br from-50% from-blue-500 via-blue-300 to-white bg-blue-300 rounded-md p-12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M1.24264 8.24264L8 15L14.7574 8.24264C15.553 7.44699 16 6.36786 16 5.24264V5.05234C16 2.8143 14.1857 1 11.9477 1C10.7166 1 9.55233 1.55959 8.78331 2.52086L8 3.5L7.21669 2.52086C6.44767 1.55959 5.28338 1 4.05234 1C1.8143 1 0 2.8143 0 5.05234V5.24264C0 6.36786 0.44699 7.44699 1.24264 8.24264Z"
+              fill="#ffffff"
+            />
+          </svg>
+        </div>
+        <span class='font-extrabold text-7xl flex items-end pb-5'>
+          Your Likes
+        </span>
+      </div>
+      <tracks />
+    </div>
     }@else {
     <main class="flex flex-col gap-20 bg-[#19181f] mb-20">
       <div class="flex flex-col items-center gap-5 ">
