444c581fbb9f01be58179e7f080519fccb75150d
Andres Laguilavo <lagui2003@gmail.com>
2024-11-06 19:45:33 -0500

    Add environment configuration setup and update build process

---
+++ frontend/src/set-env.ts
@@ -0,0 +1,36 @@
+const setEnv = () => {
+  const fs = require('fs')
+  const writeFile = fs.writeFile
+  const targetPath = './src/environments/environment.ts'
+  const colors = require('colors')
+  require('dotenv').config({
+    path: '.env'
+  })
+  const envConfigFile = `export const environment = {
+  production: true,
+  api: '${process.env['API']}',
+  spotifyClientId: '${process.env['SPOTIFY_CLIENT_ID']}',
+  redirectUri: '${process.env['REDIRECT_URI']}',
+  spotifyApiUrl: '${process.env['SPOTIFY_API_URL']}',
+};
+`
+  console.log(
+    colors.magenta(
+      'The file `environment.ts` will be written with the following content: \n'
+    )
+  )
+  writeFile(targetPath, envConfigFile, (err: unknown) => {
+    if (err) {
+      console.error(err)
+      throw err
+    } else {
+      console.log(
+        colors.magenta(
+          `Angular environment.ts file generated correctly at ${targetPath} \n`
+        )
+      )
+    }
+  })
+}
+
+setEnv()
