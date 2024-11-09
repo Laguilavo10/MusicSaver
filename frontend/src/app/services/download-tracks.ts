999f666ad23c98e29ce803ed44ad9546de04e2da
Andres Laguilavo <lagui2003@gmail.com>
2024-11-09 12:49:50 -0500

    Update downloadTracks service to use environment variable for API URL

--- frontend/src/app/services/download-tracks.ts
+++ frontend/src/app/services/download-tracks.ts
@@ -1,11 +1,11 @@
 import { Track } from '@/app/types/app'
 import { saveAs } from 'file-saver'
-
+import { environment } from '@/environments/environment'
 export const downloadTracks = async (tracks: Track[]) => {
   const accessToken = localStorage.getItem('access_token') ?? ''
   for (const { name, id } of tracks) {
     try {
-      const res = await fetch('http://127.0.0.1:8000/download', {
+      const res = await fetch(`${environment.api}/download`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
